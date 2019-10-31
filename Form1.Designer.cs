5c2e810563daf311db0a98895d35dd01c6ea2d3b
DANAN1MKA <drujochek22@gmail.com>
2019-10-31 18:46:57 +0300

    added source code

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Выключатор
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mtbHours = new System.Windows.Forms.MaskedTextBox();
+            this.mtbMinutes = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lMessage = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // mtbHours
+            // 
+            this.mtbHours.Font = new System.Drawing.Font("Bahnschrift", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mtbHours.Location = new System.Drawing.Point(84, 77);
+            this.mtbHours.Mask = "000";
+            this.mtbHours.Name = "mtbHours";
+            this.mtbHours.Size = new System.Drawing.Size(49, 46);
+            this.mtbHours.TabIndex = 0;
+            this.mtbHours.ValidatingType = typeof(int);
+            this.mtbHours.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mtbHours_KeyDown);
+            // 
+            // mtbMinutes
+            // 
+            this.mtbMinutes.Font = new System.Drawing.Font("Bahnschrift", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mtbMinutes.Location = new System.Drawing.Point(149, 77);
+            this.mtbMinutes.Mask = "000";
+            this.mtbMinutes.Name = "mtbMinutes";
+            this.mtbMinutes.Size = new System.Drawing.Size(51, 46);
+            this.mtbMinutes.TabIndex = 1;
+            this.mtbMinutes.ValidatingType = typeof(int);
+            this.mtbMinutes.TextChanged += new System.EventHandler(this.mtbMinutes_TextChanged);
+            this.mtbMinutes.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mtbMinutes_KeyDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(84, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(64, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "часы";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(149, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(61, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "минуты";
+            // 
+            // lMessage
+            // 
+            this.lMessage.AutoSize = true;
+            this.lMessage.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lMessage.ForeColor = System.Drawing.Color.Red;
+            this.lMessage.Location = new System.Drawing.Point(12, 167);
+            this.lMessage.Name = "lMessage";
+            this.lMessage.Size = new System.Drawing.Size(46, 23);
+            this.lMessage.TabIndex = 4;
+            this.lMessage.Text = "label1";
+            this.lMessage.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.ForeColor = System.Drawing.Color.Black;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Выключение",
+            "Перезагрузка",
+            "Гибернация"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 129);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(116, 31);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.UseWaitCursor = true;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.MouseEnter += new System.EventHandler(this.comboBox1_MouseEnter);
+            this.comboBox1.MouseLeave += new System.EventHandler(this.comboBox1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(284, 199);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lMessage);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mtbMinutes);
+            this.Controls.Add(this.mtbHours);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(300, 238);
+            this.MinimumSize = new System.Drawing.Size(300, 238);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Выключатор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox mtbHours;
+        private System.Windows.Forms.MaskedTextBox mtbMinutes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lMessage;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Выключатор
+{
+    public partial class Form1 : Form
+    {
+        string st = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            //textBox1.BackColor = Color.Transparent;
+            comboBox1.SelectedIndex = 0;
+
+            //st = "/s";
+            
+        }
+
+        private void mtbMinutes_KeyDown(object sender, KeyEventArgs e)
+        {
+            lMessage.Visible = false;
+            //если все текстбоксы пусты и нажат enter сообщаем об ошибке
+            if (mtbHours.Text == string.Empty &&
+                mtbMinutes.Text == string.Empty &&
+                e.KeyCode == Keys.Enter)
+            {
+                lMessage.Text = "необходимо ввести время";
+                lMessage.Visible = true;
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    int time = mtbHours.Text != string.Empty ? Convert.ToInt32(mtbHours.Text) * 3600 : 0;
+                       time += mtbMinutes.Text != string.Empty ? Convert.ToInt32(mtbMinutes.Text) * 60 : 0;
+                        
+                    if (time == 0)
+                    {
+                        string message = "Вы уверены что хотите ";
+                        switch(comboBox1.SelectedIndex)
+                        {
+                            case 0:
+                                message += "выключить компьютер";
+                                break;
+                            case 1:
+                                message += "перезагрузить компьютер";
+                                break;
+                            case 2:
+                                message += "погрузить компьютер в гибернацию";
+                                break;
+                        }
+                        message += " сейчас?";
+
+                        if(MessageBox.Show(message, 
+                                           "Выключатор спрашивает", 
+                                           MessageBoxButtons.YesNo) == DialogResult.No)
+                        { return; }
+                    }
+                    else
+                    { st += " /f /t " + Convert.ToString(time); }
+
+                    //создаем и запускаем процесс со вссеми необходимыми атрибутами
+                    Process cmd = new Process();
+                    cmd.StartInfo = new ProcessStartInfo(@"shutdown", st);
+                    cmd.StartInfo.RedirectStandardInput = true;
+                    cmd.StartInfo.UseShellExecute = false;
+                    cmd.StartInfo.CreateNoWindow = true;
+                    cmd.Start();
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void mtbMinutes_TextChanged(object sender, EventArgs e)
+        {
+            if (mtbMinutes.Text != string.Empty && Convert.ToInt32(mtbMinutes.Text) >= 60)
+            {
+                int hour = Convert.ToInt32(mtbMinutes.Text) / 60;
+                int minute = Convert.ToInt32(mtbMinutes.Text) - hour * 60;
+                //если бокс пуст помещаем в него значение если нет изменяем (использована тернарная операция)
+                mtbHours.Text = mtbHours.Text == string.Empty ? Convert.ToString(hour) : Convert.ToString(Convert.ToInt32(mtbHours.Text) + hour);
+                mtbMinutes.Text = minute == 0 ? string.Empty : Convert.ToString(minute);
+            }
+        }
+
+        private void mtbHours_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                mtbMinutes.Focus();
+        }
+
+        private void comboBox1_MouseEnter(object sender, EventArgs e)
+        {
+            //comboBox1.FlatStyle = FlatStyle.System;
+            //comboBox1.BackColor = Color.White;
+        }
+
+        private void comboBox1_MouseLeave(object sender, EventArgs e)
+        {
+            //comboBox1.FlatStyle = FlatStyle.Flat;
+            //comboBox1.BackColor = Color.White;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    st = "/s";
+                    break;
+                case 1:
+                    st = "/r";
+                    break;
+                case 2:
+                    st = "/h";
+                    break;
+
+            }
+
+        }
+    }
+}
